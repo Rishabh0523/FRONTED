8ab29468dea939459e474190b2d733639c2a54e5
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-10 18:40:58 +0530

    Update function expression.js

--- Complete - JAVASCRIPT( CWH ) + PROJECTS/First class function/function expression.js
+++ Complete - JAVASCRIPT( CWH ) + PROJECTS/First class function/function expression.js
@@ -1,1 +1,11 @@
 
+// b();
+
+// Here hoisting will play the major role and error will come because we are assigning the var b to the function 
+
+var b = function () {
+  console.log("b is called");
+}
+
+b();
+
