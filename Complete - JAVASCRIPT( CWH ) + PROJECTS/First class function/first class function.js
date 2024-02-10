2689b7f689ec157628e98e78317f1995fb688daf
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-10 18:58:03 +0530

    Update first class function.js

--- Complete - JAVASCRIPT( CWH ) + PROJECTS/First class function/first class function.js
+++ Complete - JAVASCRIPT( CWH ) + PROJECTS/First class function/first class function.js
@@ -1,1 +1,9 @@
 
+// A programming language is said to have First-class functions if functions in that language are treated like other variables. So the functions can be assigned to any other variable or passed as an argument or can be returned by another function. JavaScript treats function as a first-class citizen. This means that functions are simply a value and are just another type of object.
+
+// Usage of First-Class Function
+// It can be stored as a value in a variable.
+// It can be returned by another function.
+// It can be passed into another function as a parameter.
+// It can also stored in an array, queue, or stack.
+// Also, It can have its own methods and property.
