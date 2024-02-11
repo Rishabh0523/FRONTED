06f68f5ee9e77f883b274291a57d94d4ace39580
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-11 22:53:05 +0530

    Update Anonymous function.js

--- Complete - JAVASCRIPT( CWH ) + PROJECTS/First class function/Anonymous function.js
+++ Complete - JAVASCRIPT( CWH ) + PROJECTS/First class function/Anonymous function.js
@@ -1,8 +1,10 @@
 function () {
-
+  
+// Anonymous function are used in the place where function are used as a value .
+  
 }
 
 // Anonymyous function does'nt have their own identity . 
 // The code will give the syntax error .
 
-// Anonymous function are used in the place where function are used as a value .
+
