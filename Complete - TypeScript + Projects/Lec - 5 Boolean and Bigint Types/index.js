2af91527e1777652f05e4cfab0f8a08c53859246
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-17 17:11:26 +0530

    Add files via upload

---
+++ Complete - TypeScript + Projects/Lec - 5 Boolean and Bigint Types/index.js
@@ -0,0 +1,8 @@
+var isMyName = true;
+var isDone = false;
+function isEven(a) {
+    return a % 2 === 0;
+}
+console.log(isEven(5));
+var bignumber = 9839389383983938393n;
+console.log(bignumber);
---
+++ Complete - TypeScript + Projects/Lec - 5 Boolean and Bigint Types/index.ts
@@ -0,0 +1,12 @@
+let isMyName:boolean =true;
+let isDone:boolean = false;
+
+function isEven(a:number):boolean{
+    
+    return a % 2  === 0;
+}
+
+console.log(isEven(5));
+
+let bignumber: bigint = 9839389383983938393n;
+console.log(bignumber);
