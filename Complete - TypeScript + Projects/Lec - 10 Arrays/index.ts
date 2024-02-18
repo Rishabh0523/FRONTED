78e150f6f9720b51cb37c71b6f2298bd170de1ff
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-18 09:16:27 +0530

    Add files via upload

---
+++ Complete - TypeScript + Projects/Lec - 10 Arrays/index.ts
@@ -0,0 +1,16 @@
+
+// ( 1 ) Using square brackets
+
+// const numbers:number[] = [1,2,3,4,5];
+
+// ( 2 ) Using the Array constructor 
+
+const number1:number[] = new Array(1,2,3,4);
+
+// ( 3 ) Using the Array.of method
+
+// const names:string[] = Array.of("Rishu","Nishu","Ritik")
+
+// Accessing elements using index
+
+console.log(number1[0]);
