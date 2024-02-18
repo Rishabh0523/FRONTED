76cdf283ff8de70fedd23ca527312178c0db7abe
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-18 09:16:50 +0530

    Add files via upload

---
+++ Complete - TypeScript + Projects/Lec - 11 Arrays methods And Iteration/index.js
@@ -0,0 +1,16 @@
+// Push and pop Methods 
+var fruits = ["Apple", "mango", "Grapes", "Banana"];
+// const result = fruits.push("orange");
+// const result1 = fruits.pop();
+// console.log(result);
+// console.log(result1);
+// Using loop
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// }
+// using for... of loop
+// for(const fruits of fruits){
+//     console.log(fruits)
+// }
+// using forEach method
+fruits.forEach(function (curVal) { return console.log(curVal); });
---
+++ Complete - TypeScript + Projects/Lec - 11 Arrays methods And Iteration/index.ts
@@ -0,0 +1,25 @@
+// Push and pop Methods 
+
+const fruits:string[] = ["Apple","mango","Grapes","Banana"]
+
+// const result = fruits.push("orange");
+// const result1 = fruits.pop();
+
+// console.log(result);
+// console.log(result1);
+
+// Using loop
+
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// }
+
+// using for... of loop
+
+// for(const fruits of fruits){
+//     console.log(fruits)
+// }
+
+// using forEach method
+
+fruits.forEach((curVal:string) => console.log(curVal));
