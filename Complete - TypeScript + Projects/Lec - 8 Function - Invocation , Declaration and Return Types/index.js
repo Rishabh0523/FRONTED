be71ab04075c08103691ba3568571ad5b62f048f
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-18 09:15:47 +0530

    Add files via upload

---
+++ Complete - TypeScript + Projects/Lec - 8 Function - Invocation , Declaration and Return Types/index.js
@@ -0,0 +1,4 @@
+function greet(name, id) {
+    console.log("Welcome , ".concat(name, " and your id is ").concat(id));
+}
+greet("Rishabh", 1);
---
+++ Complete - TypeScript + Projects/Lec - 8 Function - Invocation , Declaration and Return Types/index.ts
@@ -0,0 +1,14 @@
+
+function greet(name:string , id:number) {
+    console.log(`Welcome , ${name} and your id is ${id}`);
+}
+
+greet("Rishabh",1);
+
+
+const isPalindrome  = (palin:string):boolean => {
+    let myPalin = palin.split(" ");
+    return myPalin === palin
+}
+
+console.log(myPalin);
