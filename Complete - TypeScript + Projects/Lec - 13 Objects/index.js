8a71fc3d8c0f296bd0bf1ba5eedcaf71c17c1b5c
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-19 00:06:03 +0530

    Add files via upload

---
+++ Complete - TypeScript + Projects/Lec - 13 Objects/index.js
@@ -0,0 +1,16 @@
+var person = {
+    name: "Rishabh Raj",
+    age: 22,
+    College: "LPU",
+    Address: {
+        Town: " Ramnagar",
+        state: "Bihar"
+    }
+};
+console.log(person.name);
+console.log(person.Address.Town);
+// Update the Data
+// person.Address.State = " Punjab" ;
+// console.log(person.Address.State)
+// person.Address.state = 123 ;
+console.log(person.Address.state);
---
+++ Complete - TypeScript + Projects/Lec - 13 Objects/index.ts
@@ -0,0 +1,27 @@
+const person: {
+      name : string;
+      age: number;
+      College:string;
+      Address:{Town:string; state:string}
+} ={
+    name : "Rishabh Raj",
+    age : 22,
+    College : "LPU",
+
+    Address: {
+
+    Town : " Ramnagar",
+    state : "Bihar"
+    }
+}
+
+console.log(person.name)
+console.log(person.Address.Town)
+
+// Update the Data
+
+// person.Address.State = " Punjab" ;
+// console.log(person.Address.State)
+
+// person.Address.state = 123 ;
+console.log(person.Address.state)
