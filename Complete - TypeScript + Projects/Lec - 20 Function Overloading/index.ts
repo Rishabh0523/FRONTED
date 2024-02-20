d0b83d1ff81029734c9b8e9346dd7c5aaac3dfd1
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-20 23:05:44 +0530

    Update index.ts

---
+++ Complete - TypeScript + Projects/Lec - 20 Function Overloading/index.ts
@@ -0,0 +1,10 @@
+function add(a:string, b:string):string;
+
+function add(a:number, b:number): number;
+
+function add(a: any, b:any): any {
+    return a + b;
+}
+
+add("Hello ", "Rishabh"); 
+add(10, 20); 
