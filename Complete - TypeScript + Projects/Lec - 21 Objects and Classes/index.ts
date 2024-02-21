3bb4a14e93b3b2021007174051d850a37f7fd9ee
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-02-21 22:56:00 +0530

    Add files via upload

---
+++ Complete - TypeScript + Projects/Lec - 21 Objects and Classes/index.ts
@@ -0,0 +1,36 @@
+
+
+// interface Greet{
+
+//     name:string;
+//     age:number
+// }
+
+// const greets:Greet= {
+//     name :"Rishabh",
+//     age : 29
+// }
+
+// Creating a product 
+
+interface Product {
+    name: string;
+    price:number;
+    quantity : number
+}
+
+const products:Product ={
+    name : "Macbook",
+    price : 99,
+    quantity :4
+}
+
+
+// Calculate total price 
+
+const TotalPrice : (product : any) => number = (product:Products):number => {
+    const {price,quantity} = products;
+    return product*price;
+}
+
+console.log(TotalPrice(products:products))
