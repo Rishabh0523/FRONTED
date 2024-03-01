bdf7d7fef3592b2b57d938cd04889fc6f3aefd5a
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-03-01 21:16:05 +0530

    Update index.ts

--- Complete - TypeScript + Projects/Lec - 7 Type Inference/index.ts
+++ Complete - TypeScript + Projects/Lec - 7 Type Inference/index.ts
@@ -1,5 +1,5 @@
 // Type Inference :
 
 // Type inference in Typescript refers to the ability of the Typescript compiler to automatically determine and assign types to 
-// variables , expressions , and function return values based on their usage and context in the code.
+// variables , expressions , and function and return values based on their usage and context in the code.
 
