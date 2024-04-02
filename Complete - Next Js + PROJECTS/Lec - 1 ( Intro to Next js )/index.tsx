f351cc816cbe58a79350f2be54d26f45c2cdeafd
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-02 23:14:58 +0530

    Update index.tsx

--- Complete - Next Js + PROJECTS/Lec - 1 ( Intro to Next js )/index.tsx
+++ Complete - Next Js + PROJECTS/Lec - 1 ( Intro to Next js )/index.tsx
@@ -1,4 +1,4 @@
 // Next Js is a react production framework . 
-// It gives us Server Side Rendering , static site generation , Built in Router also gives some tools for SEO .
+// It gives us Server Side Rendering , Static Site Generation , Built in Router also gives some tools for SEO .
 
 
