9f36ed776d9e61f306f289677da598896c25c1e5
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-03 17:28:15 +0530

    Add files via upload

---
+++ Complete - Next Js + PROJECTS/Lec - 3 ( First Program )/firstprogram/src/app/page.js
@@ -0,0 +1,20 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Home page </h1>
+      <User name="Rishabh"/>
+      <User name="Vishal"/>
+      <User name="Nisha"/>
+    </main>
+  );
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <p>User name is {props.name}</p>
+    </div>
+  )
+}
