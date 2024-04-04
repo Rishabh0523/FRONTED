2dc442d27cdd22d90700d899dcb34b9020e80225
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-04 22:15:05 +0530

    Add files via upload

---
+++ Complete - Next Js + PROJECTS/Lec - 9 ( Nested Routing )/nested-routing/src/app/login/loginstudent/page.js
@@ -0,0 +1,10 @@
+const LoginStudent = () => {
+    return (
+        <div>
+            <h1>Login page for students </h1>
+        </div>
+    )
+}
+
+
+export default LoginStudent;
---
+++ Complete - Next Js + PROJECTS/Lec - 9 ( Nested Routing )/nested-routing/src/app/login/loginteacher/page.js
@@ -0,0 +1,10 @@
+const LoginTeacher = () => {
+    return (
+        <div>
+            <h1>Login page for teacher </h1>
+        </div>
+    )
+}
+
+
+export default LoginTeacher;
