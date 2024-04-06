b37ad3c82666ddab7de706b87437ae5452f676a4
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-06 22:43:01 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 9 ( Nested Routing )/nested-routing/src/app/login/page.js
+++ Complete - Next Js + PROJECTS/Lec - 9 ( Nested Routing )/nested-routing/src/app/login/page.js
@@ -7,14 +7,14 @@ const Login = () => {
     const router = useRouter();
 
    const  navigate = (page) => {
-          router.push("/login/" + page)
+          router.push( "/login/" + page )
     }
     return (
         <div>
               <h1>Login Page </h1>
               <Link href="/"> Go to home page</Link>
  <br />
-              <button onClick={() => navigate("loginstudent")}>Go to Student login page</button>
+              <button onClick={() => navigate("loginstudent")}>Go to Student Login Page</button>
               <br />
               <br />
               <button onClick={() => navigate("loginteacher")}>Go to Teacher login page</button>
