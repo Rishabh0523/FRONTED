bc17f12586286423742ee87a245b26e2a3e461d4
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-09 23:53:12 +0530

    Add files via upload

---
+++ Complete - Next Js + PROJECTS/Lec - 18 ( Fetch Data in Client component) /fetch-data-in-client-Component/src/app/productlist/page.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect , useState} from "react"
+
+export default function ProductList() {
+
+    useEffect((async) => {
+        const [product , setProduct] = useState([])
+
+        let data= await fetch("https://dummyjson.com/products");
+        data = await data.json();
+        console.log(data);
+        setProduct(data.product)
+    },[])
+
+    return(
+        <div>
+            <h1>Product List</h1>
+
+            {
+                product.map((item) =>(
+                    <h3>{item.title}</h3>
+                ))
+            }
+        </div>
+    )
+}
