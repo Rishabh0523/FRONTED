00c8a1bd44658b651b39a931ed18d79910d4de41
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-09 09:57:56 +0530

    Add files via upload

---
+++ Complete - Next Js + PROJECTS/Lec - 11 ( Conditional Layout ) /conditional-layouts/src/app/about/aboutcollege/page.js
@@ -0,0 +1,10 @@
+const AboutCollege = () => {
+    return (
+        <div>
+            <h1>About page for College </h1>
+        </div>
+    )
+}
+
+
+export default AboutCollege;
---
+++ Complete - Next Js + PROJECTS/Lec - 11 ( Conditional Layout ) /conditional-layouts/src/app/login/loginstudent/page.js
@@ -0,0 +1,10 @@
+const LoginStudent = () => {
+    return (
+        <div>
+            <h1 className="heading">Login page for students </h1>
+        </div>
+    )
+}
+
+
+export default LoginStudent;
