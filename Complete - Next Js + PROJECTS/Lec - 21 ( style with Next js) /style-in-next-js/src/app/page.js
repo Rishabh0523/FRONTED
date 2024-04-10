b8dede8f96044f1c0bf7cccf2af1b1b7c5af4418
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-10 17:30:07 +0530

    Add files via upload

---
+++ Complete - Next Js + PROJECTS/Lec - 21 ( style with Next js) /style-in-next-js/src/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import styles from "./page.module.css";
+export default function Home() {
+  return (
+<main>
+<h1>Style and Css with Next Js</h1>
+<h2>Heading 2 in Main Page </h2>   
+    </main>
+  );
+}
---
+++ Complete - Next Js + PROJECTS/Lec - 21 ( style with Next js) /style-in-next-js/src/app/user/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+
+    return(
+        <div>
+            <h1 style={{backgroundColor:"blue"}}>Css with Next js</h1>
+            <h2>Heading in User Page</h2>
+        </div>
+    )
+}
