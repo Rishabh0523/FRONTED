56ea9702d7a6f8cf7cc7e3d9ca61f29414b6cbb3
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-12 21:04:53 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 24 ( Image Optimization) /image-optimization/src/app/page.js
+++ Complete - Next Js + PROJECTS/Lec - 24 ( Image Optimization) /image-optimization/src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main>
       <h1>Image Optimization</h1>
+  
       {/* <Image
       src={Profile}
       alt="nothing"
@@ -13,7 +14,7 @@ export default function Home() {
 
       <Image
       src={"https://static.toiimg.com/thumb/msid-109093664,width-400,resizemode-4/109093664.jpg"}
-      alt="no"
+      alt="Simple Image"
       height={600}
       width={600}
       />
