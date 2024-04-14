84a920e0744281ba00771f0763ac592557969e11
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-14 23:38:10 +0530

    Add files via upload

---
+++ Complete - Next Js + PROJECTS/Lec - 26 ( Generate Metadata For Dynamic Meta Data) /generate-metadata-for-dynamic-meta-data/src/app/user/page.js
@@ -0,0 +1,14 @@
+export default function Page() {
+    return (
+        <div>
+            <h2>User Page</h2>
+        </div>
+    )
+}
+
+export function generateMetadata(){
+    return{
+        title:"user page title",
+        description:"user page description"
+    }
+}
