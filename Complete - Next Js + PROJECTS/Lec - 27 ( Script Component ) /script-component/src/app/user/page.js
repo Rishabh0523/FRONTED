1b68a5c6fafc0ef085a869815cb2140a6f57dc39
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-26 23:49:16 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 27 ( Script Component ) /script-component/src/app/user/page.js
+++ Complete - Next Js + PROJECTS/Lec - 27 ( Script Component ) /script-component/src/app/user/page.js
@@ -12,7 +12,7 @@ export default function Page() {
                 console.log("File Loaded in Next Js")
             }}
             />
-            <h2>Get User Geolocation Here</h2>
+            <h2> Get Users Geolocation Here </h2>
         </div>
     )
 }
