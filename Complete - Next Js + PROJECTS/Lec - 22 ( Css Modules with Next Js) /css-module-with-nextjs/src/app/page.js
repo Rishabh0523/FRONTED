c6d0dc343a2b1727524a445fd1a0c00cc233ba97
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-04-30 21:02:42 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 22 ( Css Modules with Next Js) /css-module-with-nextjs/src/app/page.js
+++ Complete - Next Js + PROJECTS/Lec - 22 ( Css Modules with Next Js) /css-module-with-nextjs/src/app/page.js
@@ -5,7 +5,7 @@ import other from "./other.module.css"
 export default function Home() {
   return (
 <main>
-<h1 className={custom.main}>Style and Css with Next Js</h1>
+<h1 className={custom.main}>Styles and Css with Next Js</h1>
 <h2 className={other.main}>Heading 2</h2>
     </main>
   );
