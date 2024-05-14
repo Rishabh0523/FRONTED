9a5097502d7b3c7cfae33763887a0adad01cb933
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-05-14 23:08:54 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 11 ( Conditional Layout ) /conditional-layouts/src/app/about/aboutcollege/page.js
+++ Complete - Next Js + PROJECTS/Lec - 11 ( Conditional Layout ) /conditional-layouts/src/app/about/aboutcollege/page.js
@@ -1,7 +1,7 @@
 const AboutCollege = () => {
     return (
         <div>
-            <h1>About page for College </h1>
+            <h1>About page for College Student</h1>
         </div>
     )
 }
