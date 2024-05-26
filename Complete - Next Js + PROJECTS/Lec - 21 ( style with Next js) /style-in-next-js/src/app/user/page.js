3bba32776dafe3df853c17af146913fe1017cb8c
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-05-26 20:38:46 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 21 ( style with Next js) /style-in-next-js/src/app/user/page.js
+++ Complete - Next Js + PROJECTS/Lec - 21 ( style with Next js) /style-in-next-js/src/app/user/page.js
@@ -4,6 +4,7 @@ export default function Page() {
         <div>
             <h1 style={{backgroundColor:"blue"}}>Css with Next js</h1>
             <h2>Heading in Users Page</h2>
+           <p> This is a paragraph page </p>
         </div>
     )
 }
