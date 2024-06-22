fe8ca2b4402bd2c311c0ba053ed1fd96661d95b1
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-06-22 23:27:11 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 15 ( Middleware) /middleware/src/app/study/page.js
+++ Complete - Next Js + PROJECTS/Lec - 15 ( Middleware) /middleware/src/app/study/page.js
@@ -3,7 +3,7 @@ export default function Study(params) {
     console.log(params)
     return(
         <div>
-        <h1>All Lectures of College </h1>
+        <h1>All Lecture's of College </h1>
         </div>
     )
 }
