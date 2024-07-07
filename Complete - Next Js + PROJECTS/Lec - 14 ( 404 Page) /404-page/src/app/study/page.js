2a016e4125c384e8c2c40fb8293ff9b6cae01e76
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-07-07 23:40:57 +0530

    Update page.js

--- Complete - Next Js + PROJECTS/Lec - 14 ( 404 Page) /404-page/src/app/study/page.js
+++ Complete - Next Js + PROJECTS/Lec - 14 ( 404 Page) /404-page/src/app/study/page.js
@@ -4,7 +4,7 @@ export default function Study(params) {
     return(
         <div>
         <h1>All Lectures of College </h1>
-        <p>This is a paragraph page </p>
+        <p>This is a paragraph page for college students </p>
         </div>
     )
 }
